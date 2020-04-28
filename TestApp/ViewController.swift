90d55ec7c79e50f2da01838b0df98f630900db60
ravichandra-ayla <ravichandra.kr@aylanetworks.com>
2020-04-28 16:30:35 +0530

    removed unwanted lines.

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -13,20 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        //Print 1
-        //print 2
-        //print 3
-        //print 4
-        //print 5
-        //print 6
-        //print 7
-        //print 8
-        //print 9
-        //print 10
-        //print 11
-        //print 12
-        //print 13
-        //print 14
+        //print
     }
 
 
